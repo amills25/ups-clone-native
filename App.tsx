5423ed8f944c6df8ec68ba9661c78af9ae793f29
Andrew Mills <andrew@apaxsoftware.com>
2024-06-04 10:18:37 -0400

    complete project

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "./tailwind.json";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./navigator/RootNavigator";
+
+export default function App() {
+  return (
+    // @ts-ignore - TailwindProvider is missing a type definition
+    <TailwindProvider utilities={utilities}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </TailwindProvider>
+  );
+}
